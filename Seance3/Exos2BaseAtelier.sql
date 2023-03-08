16eb09fcc2cb2be2bc6c623fdf4cef6b2130ef5a
UC <ulysse.coutaud@lmba.fr>
2023-03-08 15:54:21 +0100

    maj

---
+++ Seance3/Exos2BaseAtelier.sql
@@ -0,0 +1,25 @@
+-- !!! A REMPLIR !!!
+-- Nom prenom numero_etu email
+
+-- Exercice Base De Donnée "atelier" 
+
+-- CONSIGNES:
+-- Sur votre base de donnée "atelier" (vous pouvez la modifier pour la corriger/l'améliorer)
+-- Pas d'espace dans les noms de fichiers !!!!!!!
+-- Mettez les commentaires nécessaires à la compréhension.
+-- Travail à rendre sous la forme d'une archive .zip
+-- L'archive rendue DOIT être appelé VOTRENOM_ExosAtelier2.zip
+-- L'archive rendue DOIT contenir le fichier de création de la base "atelier".
+-- Envoyer votre archive zip à ulysse.coutaud@gmail.com
+-- L'objet du mail DOIT être "L3PAII VOTRENOM Exos ateliers 2"
+
+
+
+
+-- EXERCICES:
+-- Preparer des scripts de requêtes SQL pour afficher:
+-- Faire un script qui demande à taper un type de machine, et retourne le nombre de machines de ce type.
+-- Faire un script qui demande à taper un métier, et retourne le nombre de salariés ayant ce métier.
+-- Faire un script qui demande à taper le nom d’un employé, et retourne la liste des machines sur lequel il est formé.
+-- Faire un script qui demande à taper le nom d'une machine, et retourne le nombre de machines auquel il est formé.
+-- Faire un script qui demande à taper le nom d’une machine, et retourne la liste des employes qui sont formés à l’utiliser.
