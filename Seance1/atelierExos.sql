f6b5ca21211fbd4e3dc5c3d90e3ad66e6dad7dfa
coutaudu <coutaudu@gmail.com>
2025-03-10 20:09:01 +0100

    maj exos bdd atelier

--- Seance1/atelierExos.sql
+++ Seance1/atelierExos.sql
@@ -1,49 +1,76 @@
--- Introduction aux Bases de DonnÈes 
+-- Introduction aux Bases de Donn√©es 
 -- L3P AII
 
 -- [1] Clause SELECT
--- Afficher les prÈnoms et salaires des employÈs.
--- Afficher toutes les informations des employÈs.
+-- Afficher les pr√©noms et salaires des employ√©s.
+-- Afficher toutes les informations des employ√©s.
 
 -- [2] Clause SELECT avec contraintes
--- Afficher les prÈnoms des employÈs du service production.
--- Afficher les prÈnoms des employÈs qui gagne 110 ou moins.
--- Afficher les prÈnoms des employÈs dont l'adresse contient la chaine "an".
--- Afficher les prÈnoms et salaires des employÈs du service Production et Maintenance.
+-- Afficher les pr√©noms des employ√©s du service production.
+-- Afficher les pr√©noms des employ√©s qui gagne 110 ou moins.
+-- Afficher les pr√©noms des employ√©s dont l'adresse contient la chaine "an".
+-- Afficher les pr√©noms et salaires des employ√©s du service Production et Maintenance.
 
--- [3] Filtrer les rÈsultats: Clause DISTINCT
--- Afficher sans rÈpÈtition la liste des services.
--- Afficher sans rÈpÈtition les horaires de dÈbuts de services.
+-- [3] Filtrer les r√©sultats: Clause DISTINCT
+-- Afficher sans r√©p√©tition la liste des services.
+-- Afficher sans r√©p√©tition les horaires de d√©buts de services.
 
--- [4] Filtrer les rÈsultats: Clause ORDER BY
--- Afficher toutes les informations sur les employes en les triants par ordre alphabÈtique sur les prÈnoms.
--- Afficher prÈnoms et adresses des employes en triant par salaires dÈcroissants.
+-- [4] Filtrer les r√©sultats: Clause ORDER BY
+-- Afficher toutes les informations sur les employes en les triants par ordre alphab√©tique sur les pr√©noms.
+-- Afficher pr√©noms et adresses des employes en triant par salaires d√©croissants.
 
--- [5] Filtrer les rÈsultats: Clauses LIMIT et OFFSET
--- Afficher l'employÈ le moins bien payÈ.
--- Afficher le deuxieme employÈ le moins bien payÈ.
+-- [5] Filtrer les r√©sultats: Clauses LIMIT et OFFSET
+-- Afficher l'employ√© le moins bien pay√©.
+-- Afficher le deuxieme employ√© le moins bien pay√©.
 
--- [6] RequÍtes sur plusieurs tables: Clause INNER JOIN
+-- [6] Requ√™tes sur plusieurs tables: Clause INNER JOIN
 -- Afficher les horaires de John.
--- Afficher les noms et horaires de tous les employÈs du service production.
+-- Afficher les noms et horaires de tous les employ√©s du service production.
 
--- [7] RequÍtes sur plusieurs tables: Clause LEFT JOIN
+-- [7] Requ√™tes sur plusieurs tables: Clause LEFT JOIN
 -- Afficher la jonction de toutes les informations de la BDD
--- Afficher les prÈnoms des employÈs qui n'ont pas d'horaires.
+-- Afficher les pr√©noms des employ√©s qui n'ont pas d'horaires.
 
 -- [8] Les expressions
--- Les salaires de la base sont journaliers. Afficher les prÈnoms et salaires mensuels (20 jours travaillÈs).
+-- Les salaires de la base sont journaliers. Afficher les pr√©noms et salaires mensuels (20 jours travaill√©s).
 
--- [9] Les aggrÈgats: les fonctions d'aggrÈgats
--- Afficher le nombre d'employÈs.
+-- [9] Les aggr√©gats: les fonctions d'aggr√©gats
+-- Afficher le nombre d'employ√©s.
 -- Afficher le salaire minimal.
 -- Afficher le salaire moyen.
 -- Afficher la masse salariale totale.
 
--- [9] Les aggrÈgats: Clauses GROUP BY et HAVING
+-- [9] Les aggr√©gats: Clauses GROUP BY et HAVING
 -- Afficher la masse salariale de chaque service.
 -- Afficher le salaire moyen par adresse.
--- Afficher le salaire moyen des groupes d'employÈs dont le salaire moyen du service est supÈrieur ‡ 120
+-- Afficher le salaire moyen des groupes d'employ√©s dont le salaire moyen du service est sup√©rieur √† 120
 
+------------------------------------------------------------------------------------
+-- Pour la prochaine s√©ance:
+-- Faire les requetes pour extraire les donn√©es:
+-- [1] Afficher les pr√©noms et les salaires des employ√©s du service Maintenance.
+-- [2] Afficher le salaire le plus √©lev√© du service production.
+-- [3] Afficher le salaire le plus √©lev√© de l'entreprise
+-- [4] Afficher les pr√©noms des employ√©s qui ne sont pas au service Production.
+-- [5] Afficher le salaire moyen par service.
+--
+-- Envoyer par mail √† coutaudu@gmail.com
+-- Objet du mail: [L3PAII2025] Seance_1 NomDeFamille
+-- Fichier en pi√®ce jointe: L3PAII2025_Seance1_Prenom_NomDeFamille_NumeroEtudiant.sql
+------------------------------------------------------------------------------------
 
+-- [10] Ins√©rer: Clause INSERT INTO
+-- Ajouter un nouvel employ√© dans le service "Production"
+-- Ajouter un service "Production Apr√®s-Midi" dont les horaires seront 13h-20h.
+-- Ajouter les horaires 16h-5h pour le service "Astreinte"
+-- Ajouter un nouvel employ√© "John" au service "Production Apr√®s-Midi". Que ce passe il ? 
 
+-- [11] Modifier: Clause UPDATE
+-- Augmenter le salaire des employes de "Astreinte" de 30%
+-- L'employ√© Tom change de service et passe √† l'apr√®s midi 
+-- Supprimer le service de production apr√®s-midi.
+
+-- [12] Supprimer: Clause DELETE FROM 
+-- Augmenter le salaire des employes de "Astreinte" de 30%
+-- L'employ√© Tom change de service et passe √† l'apr√®s midi 
+-- Supprimer le service de production apr√®s-midi.
