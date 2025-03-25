6a3299414832212f5d0fe6631868dd0c7134a514
unknown <coutaudu>
2025-03-25 13:45:41 +0100

    Ajoute instructions exos seance 5

---
+++ Seance5/TraceabilityExos.sql
@@ -0,0 +1,7 @@
+-- Introduction aux Bases de Données 
+-- AII 2025
+
+-- Concevez et implémentez la base de données pour recevoir les données des fichiers faultCodes.csv, ProductInfos.csv, ProductionMonitoring.csv
+
+-- Importez les données dans votre base de donnée.
+
