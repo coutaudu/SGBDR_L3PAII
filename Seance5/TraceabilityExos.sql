33b2f92b52b54a30ea4a6525c69e1750639c6218
unknown <coutaudu>
2025-03-27 18:09:27 +0100

    maj seance 5 avec BDD coherente + exos

--- Seance5/TraceabilityExos.sql
+++ Seance5/TraceabilityExos.sql
@@ -5,3 +5,26 @@
 
 -- Importez les données dans votre base de donnée.
 
+-- Faire les requetes pour 
+
+-- [1] Le nombre de lots d'inserts utilisés
+
+-- [2] Le nombre de lots de plastiques utilisées chaques jours
+
+-- [3] Le taux de rebus de la ligne
+
+-- [4] Le "Paretos" de defauts de la ligne
+
+-- [5] Les séries n'ayant jamais été produites sur la ligne
+
+-- [6] Les 10 meilleurs jours de production
+
+-- [7] Le jour ou le plus de rebus on été produits
+
+-- [8] Le jour ou le moins de pièces on été produites
+
+-- [9] Le ratio de production diametre 88 vs diametre 100
+
+-- [10] La moyenne du diametre des pièces rebutées pour default de dimension, par catégorie de diamètre.
+
+-- [11] Le nombre de pièce rebutées sans code de défaut déclaré
